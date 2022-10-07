bc1ed67d0f3fa2e5eaadd1a175a611297c51b99c
Umang-Seth <umang.delhi@gmail.com>
2022-10-07 18:45:02 +0530

    Update

--- sign_verification.py
+++ sign_verification.py
@@ -16,18 +16,19 @@ def sign_verification(img1,img2):
         if m.distance < 0.75 * n.distance:
             good.append([m])
     img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, good, None, flags=2)
-    cv2.imshow("img3", img3)
-    return len(good)
+    return img3
 
 
 img1 = cv2.imread("Photo Cheque/sign.png",0)
 img2 = cv2.imread("Photo Cheque/sampleCheque.png",0)
+img4 = cv2.imread("Photo Cheque/umang.jpg")
 
 Height = img2.shape[0]
 Width = img2.shape[1]
 img2Croppped = img2[int(Height/2):Height-100,int(Width-Width/3):Width]
 
 print(sign_verification(img1,img2Croppped))
+print(sign_verification(img4,img2Croppped))
 #img3 = cv2.drawMatchesKnn(img1,kp1,img2Croppped,kp2,good,None,flags=2)
 
 #imgKp1 = cv2.drawKeypoints(img1,kp1,None)
@@ -37,5 +38,8 @@ print(sign_verification(img1,img2Croppped))
 #cv2.imshow("kp2",imgKp2)
 cv2.imshow("img1",img1)
 cv2.imshow("img2",img2Croppped)
+cv2.imshow("img3",img4)
+cv2.imshow("img4",sign_verification(img1,img2Croppped))
+cv2.imshow("img5",sign_verification(img4,img2Croppped))
 #cv2.imshow("img3",img3)
 cv2.waitKey(0)
